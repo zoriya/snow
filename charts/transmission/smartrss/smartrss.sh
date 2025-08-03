a91bd7550dbd7d3462d186d18346249a685813e0
Zoe Roux <zoe.roux@zoriya.dev>
2025-08-04 00:13:19 +0200

    Add smartrss

---
+++ charts/transmission/smartrss/smartrss.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rsstail -f '{title}---{link}\n' "$URL" | while IFS="---" read -r title link; do
+    name=$(guessit "$title" -P title | tr -d "[:punct:]")
+
+    # shellcheck disable=SC2012
+    if ls /medias | tr -d "[:punct:]" | grep -qix "$name"; then
+        transmission-remote "$TRURL" --auth "$TRUSER:$TRPASS" -a "$link"
+    fi
+done
