8a79de8cf33880a5e2e43f527854074d9e9a9dfc
Zoe Roux <zoe.roux@zoriya.dev>
2025-08-20 01:46:24 +0200

    Add pg cluster just for immich

--- charts/meelo/ytdlp/ytdlp-sync.sh
+++ charts/meelo/ytdlp/ytdlp-sync.sh
@@ -1,9 +1,9 @@
 #!/usr/bien/env bash
 
-# while true; do
+while true; do
 	yt-dlp "$URL" -o "$OUT/%(artist)s - %(title)s.%(ext)s" \
 		--extract-audio \
 		--embed-thumbnail --embed-metadata --embed-chapters \
 		--download-archive "$OUT/ytdlp-archive.txt"
-# 	sleep 6h
-# done
+	sleep 6h
+done
