1a11aa205dada8bf9f58eb49c04796405fc2ad25
Zoe Roux <zoe.roux@zoriya.dev>
2025-08-21 09:48:04 +0200

    Fix ytdlp

--- charts/meelo/ytdlp/ytdlp-sync.sh
+++ charts/meelo/ytdlp/ytdlp-sync.sh
@@ -1,9 +1,11 @@
-#!/usr/bien/env bash
+#!/usr/bin/env bash
 
 while true; do
 	yt-dlp "$URL" -o "$OUT/%(artist)s - %(title)s.%(ext)s" \
 		--extract-audio \
 		--embed-thumbnail --embed-metadata --embed-chapters \
-		--download-archive "$OUT/ytdlp-archive.txt"
+		--download-archive "$OUT/ytdlp-archive.txt" \
+		--cookies /app/secrets/cookies.txt \
+		--cache-dir "$(pwd)"
 	sleep 6h
 done
