1f22645c7a94372d22224170b76badf87974081d
Zoe Roux <zoe.roux@zoriya.dev>
2025-08-21 09:29:46 +0200

    wip: Add ytdlp

---
+++ charts/meelo/ytdlp/ytdlp-sync.sh
@@ -0,0 +1,9 @@
+#!/usr/bien/env bash
+
+# while true; do
+	yt-dlp "$URL" -o "$OUT/%(artist)s - %(title)s.%(ext)s" \
+		--extract-audio \
+		--embed-thumbnail --embed-metadata --embed-chapters \
+		--download-archive "$OUT/ytdlp-archive.txt"
+# 	sleep 6h
+# done
