8869aca493fb95727e7378e90f1dc680f65af99d
Zoe Roux <zoe.roux@zoriya.dev>
2025-09-10 14:09:20 +0200

    Update smartrss.sh

--- charts/transmission/smartrss/smartrss.sh
+++ charts/transmission/smartrss/smartrss.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
-set -x
 
 rsstail -f '{title};{link}\n' "$URL" | while IFS=';' read -r title link; do
     name=$(guessit "$title" -P title | tr -d "[:punct:]")
 
     # shellcheck disable=SC2012
     if ls /medias | tr -d "[:punct:]" | grep -qix "$name"; then
+        echo "Adding $title (match with $name)"
         transmission-remote "$TRURL" --auth "$TRUSER:$TRPASS" -a "$link" -w "/medias/$name"
     fi
 done
