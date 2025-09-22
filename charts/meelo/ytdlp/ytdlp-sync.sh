829ab56775fce65ba977a2069bb0485a55ab8df3
Zoe Roux <zoe.roux@zoriya.dev>
2025-09-22 12:16:35 +0200

    Use cronjobs instead of while loops

--- charts/meelo/ytdlp/ytdlp-sync.sh
+++ charts/meelo/ytdlp/ytdlp-sync.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-while true; do
-	yt-dlp "$LIVES_URL" -o "$LIVES_OUT/%(title)s.%(ext)s" \
-		--embed-thumbnail --embed-metadata --embed-chapters \
-		--download-archive "$LIVES_OUT/ytdlp-archive.txt" \
-		--cookies /app/secrets/cookies.txt \
-		--cache-dir "$(pwd)"
+yt-dlp "$LIVES_URL" -o "$LIVES_OUT/%(title)s.%(ext)s" \
+	--embed-thumbnail --embed-metadata --embed-chapters \
+	--download-archive "$LIVES_OUT/ytdlp-archive.txt" \
+	--cookies /app/secrets/cookies.txt \
+	--cache-dir "$(pwd)"
 
-	yt-dlp "$URL" -o "$OUT/%(artist)s - %(title)s.%(ext)s" \
-		--extract-audio \
-		--embed-thumbnail --embed-metadata --embed-chapters \
-		--download-archive "$OUT/ytdlp-archive.txt" \
-		--cookies /app/secrets/cookies.txt \
-		--cache-dir "$(pwd)"
-	sleep 6h
-done
+yt-dlp "$URL" -o "$OUT/%(artist)s - %(title)s.%(ext)s" \
+	--extract-audio \
+	--embed-thumbnail --embed-metadata --embed-chapters \
+	--download-archive "$OUT/ytdlp-archive.txt" \
+	--cookies /app/secrets/cookies.txt \
+	--cache-dir "$(pwd)"
